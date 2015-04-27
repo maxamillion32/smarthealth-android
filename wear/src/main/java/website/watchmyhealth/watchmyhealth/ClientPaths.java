b881fa6f6e2a084c383aad53611451fdc4f743cc
Yoann Moise <moise.yoann@gmail.com>
2015-04-27 14:25:58 +0200

    Wear first version

---
+++ wear/src/main/java/website/watchmyhealth/watchmyhealth/ClientPaths.java
@@ -0,0 +1,6 @@
+package website.watchmyhealth.watchmyhealth;
+
+public class ClientPaths {
+    public static final String START_MEASUREMENT = "/start";
+    public static final String STOP_MEASUREMENT = "/stop";
+}
